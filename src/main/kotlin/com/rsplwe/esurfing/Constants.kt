b8c0a2aacf4eba501b43e689fdd9d8978ea2ecd2
Rsplwe <i@rsplwe.com>
2023-06-13 20:59:16 +0800

    update native library to 2082

--- src/main/kotlin/com/rsplwe/esurfing/Constants.kt
+++ src/main/kotlin/com/rsplwe/esurfing/Constants.kt
@@ -3,7 +3,7 @@ package com.rsplwe.esurfing
 object Constants {
 
     const val PACKAGE_ID = "com.cndatacom.campus.cdccportalgd"
-    const val USER_AGENT = "CCTP/Android8_vpn/2081"
+    const val USER_AGENT = "CCTP/Android8_vpn/2082"
     const val REQUEST_ACCEPT = "text/html,text/xml,application/xhtml+xml,application/x-javascript,*/*"
     const val BASE_URL = "http://14.146.227.141:7001"
     const val PORTAL_NODE = "http://125.88.59.131:10002"
