947ac8aac83c308545782ae123abff77fec5e210
250king <2789260300@qq.com>
2024-05-09 01:21:52 +0800

    Change generate_204 source

--- src/main/kotlin/com/rsplwe/esurfing/Constants.kt
+++ src/main/kotlin/com/rsplwe/esurfing/Constants.kt
@@ -9,6 +9,6 @@ object Constants {
     const val REQUEST_ACCEPT = "text/html,text/xml,application/xhtml+xml,application/x-javascript,*/*"
     const val BASE_URL = "http://14.146.227.141:7001"
     const val PORTAL_NODE = "http://61.140.12.23:10002"
-    const val CAPTIVE_URL = "http://www.gstatic.com/generate_204"
+    const val CAPTIVE_URL = "http://connect.rom.miui.com/generate_204"
     const val AUTH_URL = "$BASE_URL/auth.cgi"
 }
